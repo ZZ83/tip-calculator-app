3dfe00c1a93729cdebd8b3f800eec37559b4b5f9
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-07-06 18:49:36 -0700

    Add event listeners

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-document.querySelector(".tip__button").addEventListener("click", function () {
-    console.log("Testing");
-})
+let bill = 0;
+let tipAmmount = 0;
+let tipPercentage = 0;
+let totalPerPerson = 0
+let numberOfPeople = 0;
+
+const billInput        = document.querySelector("#bill");
+const customInput      = document.querySelector(".custom");
+const resetButton      = document.querySelector(".tip__button");
+const percentButtons   = document.querySelector(".percent");
+const numOfPeopleInput = document.querySelector("#people");
+
+billInput.addEventListener("keyup",  function() {
+    const inputValue = billInput.value;
+    if (isFinite(inputValue)) {
+        console.log(inputValue)
+    }
+});
+
+customInput.addEventListener("keyup",  function() {
+    console.log("Custom Tip Percentage");
+});
+
+resetButton.addEventListener("click",  function() {
+    console.log("Reset Button");
+});
+
+percentButtons.addEventListener("click",  function(event) {
+    if(event.target.tagName === "BUTTON") {
+        tipPercentage = parseInt( event.target.innerHTML.slice(0, -1) );
+    }
+});
+
+numOfPeopleInput.addEventListener("keyup",  function() {
+    console.log("Number of People");
+});
