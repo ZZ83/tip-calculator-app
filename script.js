cdff7f18998fa96bc9e06dfb7a0cb1a624b6f6db
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-07-07 19:47:15 -0700

    Add active state functionality

--- script.js
+++ script.js
@@ -10,6 +10,21 @@ const resetButton      = document.querySelector(".tip__button");
 const percentButtons   = document.querySelector(".percent");
 const numOfPeopleInput = document.querySelector("#people");
 
+function addActiveClass(element) {
+    element.classList.add("active");
+}
+
+function removeActiveClass() {
+    const sortItems = document.querySelectorAll(".btn");
+    sortItems.forEach(item => {
+        item.classList.remove("active");
+    })
+};
+
+resetButton.addEventListener("click",  function() {
+    console.log("Reset Button");
+});
+
 billInput.addEventListener("keyup",  function() {
     const inputValue = billInput.value;
     if (isFinite(inputValue)) {
@@ -18,19 +33,23 @@ billInput.addEventListener("keyup",  function() {
 });
 
 customInput.addEventListener("keyup",  function() {
-    console.log("Custom Tip Percentage");
-});
-
-resetButton.addEventListener("click",  function() {
-    console.log("Reset Button");
+    tipPercentage = parseInt(customInput.value);
+    tipPercentage = tipPercentage / 100;
+    console.log(tipPercentage);
 });
 
 percentButtons.addEventListener("click",  function(event) {
+    removeActiveClass();
     if(event.target.tagName === "BUTTON") {
+        addActiveClass(event.target);
         tipPercentage = parseInt( event.target.innerHTML.slice(0, -1) );
+        tipPercentage = tipPercentage / 100;
+        console.log(tipPercentage);
     }
 });
 
 numOfPeopleInput.addEventListener("keyup",  function() {
-    console.log("Number of People");
+    console.log(numOfPeopleInput.value0);
 });
+
+
