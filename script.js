10fbc826f8d9c3a5d68d7e52fcc1f82ba85cd2c6
ZZ83 <13670661+ZZ83@users.noreply.github.com>
2022-07-10 21:58:30 -0700

    Start adding error message functionality

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let bill = 0;
 let tipAmmount = 0;
 let tipPercentage = 0;
 let totalPerPerson = 0
-let numberOfPeople = 0;
+let numberOfPeople = 1;
 
 const billInput        = document.querySelector("#bill");
 const customInput      = document.querySelector(".custom");
@@ -10,6 +10,8 @@ const resetButton      = document.querySelector(".tip__button");
 const percentButtons   = document.querySelector(".percent");
 const numOfPeopleInput = document.querySelector("#people");
 
+
+//FUNCTIONS ------------------------------------------------------------------------
 function addActiveClass(element) {
     element.classList.add("active");
 }
@@ -21,17 +23,12 @@ function removeActiveClass() {
     })
 };
 
-resetButton.addEventListener("click",  function() {
-    console.log("Reset Button");
-});
+function validate(event) {
+    const t = event.value;
+    event.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
+}
 
-billInput.addEventListener("keyup",  function() {
-    const inputValue = billInput.value;
-    if (isFinite(inputValue)) {
-        console.log(inputValue)
-    }
-});
-
+//EVENT LISTENERS ------------------------------------------------------------------
 customInput.addEventListener("keyup",  function() {
     tipPercentage = parseInt(customInput.value);
     tipPercentage = tipPercentage / 100;
@@ -48,8 +45,287 @@ percentButtons.addEventListener("click",  function(event) {
     }
 });
 
+
+
 numOfPeopleInput.addEventListener("keyup",  function() {
-    console.log(numOfPeopleInput.value0);
+    if (isFinite(numOfPeopleInput.value)) {
+        console.log(numOfPeopleInput.value)
+
+
+
+        if(numOfPeopleInput.value === "0" ) {
+            showError();
+            console.log("Please type a number Greater then 0");
+            numOfPeopleInput.setAttribute("maxlength", "1")
+        }
+      7
+    }
+});
+
+resetButton.addEventListener("click",  function() {
+    console.log("Reset Button");
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validCharatersOnly(input) {
+    // Prevents unvalid charaters from being typed into the input field
+    const splitValue     = input.value.split('');
+    const validCharaters = new RegExp('\\d|\\.');
+    const filteredSplitValue = splitValue.map(function(character) {
+        if(!validCharaters.test(character)) {
+            return '';
+        }
+        return character;
+    });
+    
+    let x = filteredSplitValue.join('');
+
+    if (x[0] === "." && x[x.length -1] === "." && x.length > 1) {
+        x = x.slice(0, -1);
+        input.value = x.join("");
+    } else if (x[x.length -1] === "." && x[0] !== ".") {
+        let y = x.slice(0, -1);
+        if(y.includes(".")) {
+            x = x.slice(0, -1);
+            input.value = x.join("");
+        }
+    }
+}
+
+
+
+
+function checkIfEmpty()  {
+    if(numOfPeopleInput.value === "" || numOfPeopleInput.value === 0) {
+        console.log("Show Error Message");
+    }
+}
+
+function showError() {
+    numOfPeopleInput.style.border = "2px solid #E17457";
+    numOfPeopleInput.previousElementSibling.classList.add("error");
+    numOfPeopleInput.previousElementSibling.firstElementChild.style.display = "initial";
+}
+
+
+
+
+
+
+
+
+
+  
+billInput.addEventListener("input", function(event) {
+    validate(billInput);
+    validCharatersOnly(billInput)
+
+    checkIfEmpty();
+    showError();
+
+});
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function numbersOnly(inputVal) {
+//     var digitPeriodRegExp = new RegExp('\\d|\\.');
+//     var splitValue = inputVal.value.split('');
+//     var charactersToFilter = 0;
+//     var filteredSplitValue = splitValue.map(function(character) {
+//         if(!digitPeriodRegExp.test(character)) {
+//             charactersToFilter++;
+//             return '';
+//         }
+//         return character;
+//     });
+//     if(!charactersToFilter) {
+//         return;
+//     }
+//     inputVal.value = filteredSplitValue.join('');
+// }
+
+
+// function preventInvalidCharaters(event) { 
+//     const invalidChararaters = ["-", "+", "e", "E"];
+//     if (invalidChararaters.includes(event.key)) {
+//         event.preventDefault();
+//     }
+// }
+
+
+
+// billInput.addEventListener("input",  function(event) {
+//     const inputValue = billInput.value;
+//     if (isFinite(inputValue)) {
+//         console.log(inputValue)
+//         billInput.value = inputValue;
+//     }
+
+// });
+
+
+// const invalidChararaters = ["-", "+", "e", "E"];
+// var splitValue = inputVal.value.split('');
+// var filteredSplitValue = splitValue.map(function(character) {
+//     if(!invalidChararaters.includes(character)) {
+    
+//         console.log(character);
+//         // return character;
+//     }
+// });
